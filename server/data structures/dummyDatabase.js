3937d9cd0e7f9e70c8196c92caf9938841cf5512
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2018-07-15 03:44:33 +0100

    create dummy database using data structure

---
+++ server/data structures/dummyDatabase.js
@@ -0,0 +1,63 @@
+const dummyDatabase = [{
+  id: 1,
+  title: 'my first database',
+  description: 'My first database is postgresql',
+  privacy: 'public'
+},
+{
+  id: 2,
+  title: 'my name',
+  description: 'My name is chinemelu',
+  privacy: 'private'
+},
+{
+  id: 3,
+  title: 'my age',
+  description: 'I am 100 years old',
+  privacy: 'public'
+},
+{
+  id: 4,
+  title: 'family',
+  description: 'There are five people in my family',
+  privacy: 'public'
+},
+{
+  id: 5,
+  title: 'Your secondary school',
+  description: 'My secondary school is ISL',
+  privacy: 'private'
+},
+{
+  id: 6,
+  title: 'my primary school',
+  description: 'My primary school is MCPS',
+  privacy: 'public'
+},
+{
+  id: 7,
+  title: 'my university',
+  description: 'I studied at Heriot Watt University',
+  privacy: 'private'
+},
+{
+  id: 8,
+  title: 'my life goal',
+  description: 'To be successful and serve God all my days',
+  privacy: 'private'
+},
+{
+  id: 9,
+  title: 'Favourite action movie',
+  description: 'My favourite action movie is the matrix',
+  privacy: 'public'
+},
+{
+  id: 10,
+  title: 'Favourite food',
+  description: 'My favourite food is fried rice',
+  privacy: 'public'
+},
+];
+
+export default dummyDatabase;
