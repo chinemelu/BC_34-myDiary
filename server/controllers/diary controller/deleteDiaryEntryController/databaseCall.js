ef45a876a0fd738b8c83767c126964e639bd0c13
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2018-07-17 07:37:39 +0100

    feat(deleteDiaryEntry): add DELETE api/v1/entries/<entry Id> API route and the corresponding controller [Delivers #159032297]

---
+++ server/controllers/diary controller/deleteDiaryEntryController/databaseCall.js
@@ -0,0 +1,28 @@
+import dummyDatabase from '../../../data structures/dummyDatabase';
+import validIdValidator from '../../../validators/validIdValidator';
+
+const deleteDiaryEntryDatabaseCall = (req, res, callback) => {
+  const { id } = req.params; // Get id from params
+  // Validate the id and check if it's an integer - change to check for UUID later
+  // remember to write test reflecting use of UUID later
+  const { errors, isValid } = validIdValidator(req.params);
+  // find selectedEntry by using the params id to filter the dummyDatabase array
+  const selectedEntry = dummyDatabase.filter(entry => parseInt(id, 0) === entry.id);
+  const index = dummyDatabase.indexOf(selectedEntry[0]);
+  if (isValid && index !== -1) { // if there are no validation errors and object is found
+    dummyDatabase.splice(index, 1); //  remove object from dummyDatabase
+  }
+  if (isValid && selectedEntry.length === 1) {
+    callback(selectedEntry[0]);
+  // if data doesn't exist, give 404 status and error message of not found
+  } else if (isValid && selectedEntry.length === 0) {
+    res.status(404).json({
+      message: 'Diary entry not found'
+    });
+  } else { // if validation error, give error message
+    res.status(400).json({
+      errors: errors.id
+    });
+  }
+};
+export default deleteDiaryEntryDatabaseCall;
--- server/routes/diaryRoutes.js
+++ server/routes/diaryRoutes.js
@@ -3,24 +3,23 @@ import addDiaryEntryController from '../controllers/diary controller/addDiaryEnt
 import getAllDiaryEntriesController from '../controllers/diary controller/getAllDiaryEntriesController/getAllDiaryEntriesController';
 import getSingleDiaryEntryController from '../controllers/diary controller/getSingleDiaryEntryController/getSingleDiaryEntryController';
 import modifyDiaryEntryController from '../controllers/diary controller/modifyDiaryEntryController/modifyDiaryEntryController';
-
+import deleteDiaryEntryController from '../controllers/diary controller/deleteDiaryEntryController/deleteDiaryEntryController';
 
 const router = express.Router();
 
 router.post(
   '/', addDiaryEntryController.addEntry
 );
-
 router.get(
   '/', getAllDiaryEntriesController.getAllEntries
 );
-
 router.get(
   '/:id', getSingleDiaryEntryController.getSingleEntry
 );
-
 router.put(
   '/:id', modifyDiaryEntryController.modifyEntry
 );
-
+router.delete(
+  '/:id', deleteDiaryEntryController.deleteEntry
+);
 export default router;
