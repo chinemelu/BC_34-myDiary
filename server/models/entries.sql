831a37c06249583a5ab3f6e079baeab662711a58
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2018-07-26 19:28:20 +0100

    modify scripts to make travis CI create entries table

--- server/models/entries.sql
+++ server/models/entries.sql
@@ -3,7 +3,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE entries (
   id uuid NOT NULL DEFAULT uuid_generate_v4(),
-  userid uuid NOT NULL DEFAULT uuid_generate_v4(),
+  userid uuid DEFAULT uuid_generate_v4(),
   title varchar(255) NOT NULL,
   description varchar(255) NOT NULL UNIQUE,
   privacy VARCHAR(255) NOT NULL,
