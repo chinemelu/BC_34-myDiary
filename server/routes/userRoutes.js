93d41ec01395b692ecb9751f991eb9d533aa8420
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2018-07-26 07:48:24 +0100

    update user route with the login API route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import userSignupController from '../controllers/user controller/user signup/signup';
+import userLoginController from '../controllers/user controller/user login/login';
 import signupValidator from '../validators/user validator/sign up validator/index';
 
 
 const router = express.Router();
 
 router.post('/signup', signupValidator, userSignupController.signup);
+router.post('/login', userLoginController.login);
 
 export default router;
