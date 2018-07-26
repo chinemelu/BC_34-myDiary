9ffab2b1c2f2f991312c34e028d7880b5d587256
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2018-07-26 19:04:48 +0100

    modify files and tests

--- server/test/Diary/getSingleDiaryEntry/getSingleDiaryEntryTest.js
+++ server/test/Diary/getSingleDiaryEntry/getSingleDiaryEntryTest.js
@@ -8,36 +8,25 @@ chai.use(chaiHttp);
 describe('diary entries', () => {
   describe('GET: /api/v1/entries/<entry id>', () => {
     it('should get the details of a SINGLE diary entry when there are no errors', (done) => {
-      const diaryEntry = {
-        id: 11,
-        title: 'A girl has no name',
-        description: 'Game of Thrones redefined',
-        privacy: 'public'
-      };
       chai.request(server)
-        .post('/api/v1/entries')
-        .send((diaryEntry))
-        .end(() => {
-          chai.request(server)
-            .get('/api/v1/entries/11')
-            .end((err, res) => {
-              res.should.have.status(200);
-              res.should.be.json;
-              res.body.should.be.a('object');
-              res.body.should.have.property('data');
-              res.body.data.should.have.property('id');
-              res.body.data.id.should.be.a('number');
-              res.body.data.should.have.property('title');
-              res.body.data.title.should.be.a('string');
-              res.body.data.title.should.eql('A girl has no name');
-              res.body.data.should.have.property('description');
-              res.body.data.description.should.be.a('string');
-              res.body.data.description.should.eql('Game of Thrones redefined');
-              res.body.data.should.have.property('privacy');
-              res.body.data.privacy.should.be.a('string');
-              res.body.data.privacy.should.eql('public');
-              done();
-            });
+        .get('/api/v1/entries/11')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          res.body.should.have.property('data');
+          res.body.data.should.have.property('id');
+          res.body.data.id.should.be.a('number');
+          res.body.data.should.have.property('title');
+          res.body.data.title.should.be.a('string');
+          res.body.data.title.should.eql('A girl has no name');
+          res.body.data.should.have.property('description');
+          res.body.data.description.should.be.a('string');
+          res.body.data.description.should.eql('Game of Thrones redefined');
+          res.body.data.should.have.property('privacy');
+          res.body.data.privacy.should.be.a('string');
+          res.body.data.privacy.should.eql('public');
+          done();
         });
     });
   });
--- server/test/User/signup/validation/username/usernameExists.js
+++ server/test/User/signup/validation/username/usernameExists.js
@@ -1,41 +1,41 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import server from '../../../../../app';
-import pool from '../../../../../models/db';
-
 
 chai.should();
 chai.use(chaiHttp);
 
 describe('POST: /api/v1/auth/signup', () => {
-  before((done) => {
-    const query = "INSERT INTO users(username, email, password) VALUES ('teejay2k4', 'tony@yahoo.com','iamawesome')";
-    pool.connect() // connect to database
-      .then(client => client.query(query)
-        .then(() => done())
-        .catch(() => {
-          client.release();
-        }));
-  });
   it('should respond with a conflict error if the username already exists', (done) => {
     const registrationDetails = {
       username: 'teejay2k4',
       email: 'teejay2k4@yahoo.com',
       password: 'iAMAwesome'
     };
+    const newRegistrationDetails = {
+      username: 'teejay2k4', // same username
+      email: 'teejay@yahoo.com', // different emails
+      password: 'iAMAwesome'
+    };
     chai.request(server)
       .post('/api/v1/auth/signup')
       .send(registrationDetails)
       .end((err, res) => {
-        res.should.have.status(409);
-        res.should.be.json;
-        res.body.should.be.a('object');
-        res.body.should.have.property('errors');
-        res.body.errors.should.be.a('object');
-        res.body.errors.should.have.property('usernameExists');
-        res.body.errors.usernameExists.should.be.a('string');
-        res.body.errors.usernameExists.should.eql('username exists');
-        done();
+        res.should.have.status(201);
+        chai.request(server)
+          .post('/api/v1/auth/signup')
+          .send(newRegistrationDetails)
+          .end((err, res) => {
+            res.should.have.status(409);
+            res.should.be.json;
+            res.body.should.be.a('object');
+            res.body.should.have.property('errors');
+            res.body.errors.should.be.a('object');
+            res.body.errors.should.have.property('usernameExists');
+            res.body.errors.usernameExists.should.be.a('string');
+            res.body.errors.usernameExists.should.eql('username exists');
+            done();
+          });
       });
   });
 });
