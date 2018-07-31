ab99d1d0cdb24e7b6f35708878bd74286750dcbd
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2018-07-31 04:41:37 +0100

    modify controllers to solve postgresql and postman timeout issue

--- server/controllers/diary controller/addDiaryEntryController/databaseCall.js
+++ server/controllers/diary controller/addDiaryEntryController/databaseCall.js
@@ -1,22 +1,26 @@
-import pool from '../../../models/db';
+import { Pool } from 'pg';
+import dbDetails from '../../../models/db';
 import isPrivacyFieldEmpty from './isPrivacyEmpty';
 
-const addDiaryEntryDatabaseCall = (req, res, data, userId) => {
-  data.privacy = isPrivacyFieldEmpty(data); // if privacy field is empty, default value is private
-  pool.connect() // connect to database
-    .then(client => client.query('INSERT INTO entries(title, description, privacy, userid) VALUES ($1, $2, $3, $4) RETURNING \n'
-    + 'id, title, description, privacy',
-    [req.body.title, req.body.description, req.body.privacy, userId])
-      .then((result) => {
-        res.status(201).json({
-          message: 'You have successfully posted the diary entry',
-          data: result.rows[0]
-        });
-        client.release();
-      })
-      .catch((err) => {
-        res.status(500).json({ error: err.stack });
-        client.release();
-      }));
+const addDiaryEntryDatabaseCall = (req, res, userId) => {
+  // if privacy field is empty, default value is private
+  const pool = new Pool(dbDetails);
+  req.body.privacy = isPrivacyFieldEmpty(req.body);
+
+  const query = 'INSERT INTO entries(title, description, privacy, userid) VALUES ($1, $2, $3, $4) RETURNING \n'
+  + 'id, title, description, privacy';
+
+  const values = [req.body.title, req.body.description, req.body.privacy, userId];
+
+
+  pool.query(query, values, (err, result) => {
+    if (err) {
+      return res.status(500).json({ error: err.stack });
+    }
+    res.status(201).json({
+      message: 'You have successfully posted the diary entry',
+      data: result.rows[0]
+    });
+  });
 };
 export default addDiaryEntryDatabaseCall;
--- server/controllers/diary controller/getSingleDiaryEntryController/getSingleDiaryEntryController.js
+++ server/controllers/diary controller/getSingleDiaryEntryController/getSingleDiaryEntryController.js
@@ -1,7 +1,7 @@
 import getSingleDiaryEntryDatabaseCall from './databaseCall';
-import validIdValidator from '../../../validators/shared/validIdValidator/validIdValidator';
-import authenticateToken from '../../../authentication/authenticateToken';
 import authenticateUser from '../../../authentication/authenticateUser';
+import authenticateToken from '../../../authentication/authenticateToken';
+import doesUserExist from '../../../authentication/doesUserExist';
 
 /**
  * @class diarycontroller
@@ -14,24 +14,18 @@ class diarycontroller {
      * @returns {JSON} returns a JSON object
      */
   static getSingleEntry(req, res) {
-    // Validate the id and check if it's a UUID
-    const { errors, isValid } = validIdValidator(req.params);
-
-    if (isValid) { // if there are no validation errors, authenticate user via token
-      authenticateToken(req, res, (decodedToken) => {
-        const { userId } = decodedToken; // get userId from decoded token
-        const { id } = req.params; // get id from params
-
-        getSingleDiaryEntryDatabaseCall(req, res, id, (diaryEntry) => {
-          // function for validating if user can perform action
-          authenticateUser(userId, diaryEntry, res);
+    authenticateToken(req, res, (decodedToken) => {
+      const { userId } = decodedToken;
+      doesUserExist(res, userId, (verifiedExistingUserId) => {
+        getSingleDiaryEntryDatabaseCall(req, res, (diaryEntry) => {
+          authenticateUser(verifiedExistingUserId, diaryEntry, res, () => {
+            res.status(200).send({
+              data: diaryEntry
+            });
+          });
         });
       });
-    } else {
-      res.status(400).json({
-        errors: errors.id
-      });
-    }
+    });
   }
 }
 
